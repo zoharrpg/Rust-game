3482eed3650e2e6b9c62d15c5fb7c886676df3bb
Zohar <juj22@pitt.edu>
2021-12-10 22:10:56 -0500

    Finish

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+
+fn main() {
+    println!("Guess the number");
+    let secret=rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
+
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret)
+        {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too large"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+
+        }
+    }
+
+}
